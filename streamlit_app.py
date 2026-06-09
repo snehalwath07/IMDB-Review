5f0441cbb574081d33ed5fae57b20b4b95f94385
snehalwath07 <snehalwath782@gmail.com>
2026-06-09 11:55:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ from nltk.tokenize import word_tokenize
 nltk.download('stopwords', quiet=True)
 nltk.download('punkt', quiet=True)
 nltk.download('wordnet', quiet=True)
-nltk.download('punkt_tab', quiet=True)
+
 
 # Page configuration
 st.set_page_config(
